7921b9f7a4c505e895c34121b27be06a2907cc25
Tirthesh Jain <tirtheshjaintj@gmail.com>
2025-05-25 12:56:01 +0530

    Tirthesh jain

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useInternships } from "./context/InternshipProvider";
 import type { Filters, Internship } from "./types";
 import { ToastContainer } from "react-toastify";
@@ -8,7 +8,6 @@ import Internships from "./components/Internships";
 
 const App: React.FC = () => {
   const { internships } = useInternships();
-  const [filteredInternships, setFilteredInternships] = useState<Internship[]>(internships);
   const [filters, setFilters] = useState<Filters>({
     workFromHome: false,
     partTime: false,
@@ -26,52 +25,47 @@ const App: React.FC = () => {
   const [profileSuggestion,setProfileSuggestion]=useState<string[]>([]);
   const [locationSuggestion,setLocationSuggestion]=useState<string[]>([]);
 
-  useEffect(() => {
+  const filteredInternships = useMemo<Internship[]>(() => {
     let result = internships;
 
-    if (filters.workFromHome){
+    if (filters.workFromHome) {
       result = result.filter((i) => i.work_from_home);
     }
-    if (filters.partTime){
+    if (filters.partTime) {
       result = result.filter((i) => i.part_time);
     }
-    if (filters.stipend >=0){
-      result = result.filter((i) => i.stipend.salaryValue1 >= (10000-filters.stipend));
+    if (filters.stipend >= 0) {
+      result = result.filter((i) => i.stipend.salaryValue1 >= (10000 - filters.stipend));
     }
-    if (filters.startDate){
+    if (filters.startDate) {
       result = result.filter((i) => new Date(i.start_date1) >= new Date(filters.startDate));
     }
-    if (filters.duration){
+    if (filters.duration) {
       result = result.filter((i) =>
         parseInt(i.duration) <= parseInt(filters.duration)
       );
     }
-
-    if (filters.keyword){
+    if (filters.keyword) {
       result = result.filter((i) =>
-        `${i.profile_name} ${i.company_name} ${i.title} `.toLowerCase().includes(filters.keyword.toLowerCase())
+        `${i.profile_name} ${i.company_name} ${i.title}`.toLowerCase().includes(filters.keyword.toLowerCase())
+      );
+    }
+    if (filters.jobOffer) {
+      result = result.filter((i) => i.is_ppo);
+    }
+    if (filters.forWomen) {
+      result = result.filter((i) => i.job_segments.includes("internship_for_women"));
+    }
+    if (filters.profiles.length > 0) {
+      result = result.filter((i) => filters.profiles.includes(i.profile_name));
+    }
+    if (filters.locations.length > 0) {
+      result = result.filter((i) =>
+        i.location_names.some((loc) => filters.locations.includes(loc))
       );
     }
 
-    if (filters.jobOffer){
-      result = result.filter((i) => i.is_ppo);
-    }
-    if (filters.forWomen){
-      result = result.filter((i) => i.job_segments.includes("internship_for_women"));
-    }
-
-   if (filters.profiles.length > 0) {
-    result = result.filter((i) =>
-      filters.profiles.includes(i.profile_name)
-    );
-   }
-
-  if (filters.locations.length > 0) {
-    result = result.filter((i) =>
-      i.location_names.some((loc) => filters.locations.includes(loc))
-    );
-  }
-    setFilteredInternships(result);
+    return result;
   }, [filters, internships]);
 
   useEffect(() => {
